d9da9dbaf0af0d8f8d1d6d320682a1a9d9063f33
Cat Plus Plus <piotrlegnica@piotrl.pl>
2015-02-22 14:48:06 +0100

    Merged assets into the site repo.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -8,9 +8,4 @@ module CustomHelpers
 
         return local_title + 'Lounge&lt;C++&gt;'
     end
-
-    def emote(code)
-        name = data.emots[code]
-        return image_tag "//assets.loungecpp.net/img/emote/#{name}", alt: code
-    end
 end
