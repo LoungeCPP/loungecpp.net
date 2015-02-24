87b22243d3e6ea377acdc944eccf4d4aed08cdda
Cat Plus Plus <piotrlegnica@piotrl.pl>
2015-02-24 16:25:07 +0100

    Get rid of relative links.

--- config.rb
+++ config.rb
@@ -33,6 +33,5 @@ configure :build do
     activate :minify_css
     activate :minify_javascript
     activate :asset_hash, :ignore => ['favicon.png']
-    activate :relative_assets
     activate :proofer
 end
